a74540cfe6d2d8b31c0feae0f08a612794527a0f
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-08-29 19:31:05 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {label: 'Add Note'},
+            {
+                label: 'Quit',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({});
+}
+
+
+
+
+
+
+
+
+
+
