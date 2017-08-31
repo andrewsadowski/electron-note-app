7b0096a2f39f19011d504274b44763391fb17fbb
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-08-30 18:14:33 -0700

    Second Window support added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -12,11 +13,22 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Note'
+    });
+}
+
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            {label: 'Add Note'},
+            {
+                label: 'Add Note',
+                click() { createAddWindow(); }
+            },
             {
                 label: 'Quit',
                 accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
